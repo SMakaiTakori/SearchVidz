c9e4ca696ab26d6193dda2d949da3c2c622ffa14
SMakaiTakori <makait87@icloud.com>
2019-06-24 18:15:49 -0400

    fixed GET request

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-$(document).ready (function(){
 // Searchbar Handler
-    $(function(){
-        var searchField=$('#query');
-        var icon = $('#search-btn');
+$(function(){
+    var searchField= $('#query');
+    var icon = $('#search-btn');
 
  // Focus Event Handler
- $(searchField).on('focus', function(){
+    $(searchField).on('focus', function(){
      $(this).animate({
         width:'100%'
         },400);
@@ -14,49 +13,49 @@ $(document).ready (function(){
         },400);
     });
 
- // Blur Event Handler
-$(searchField).on('blur', function(){
-    if(searchField.val() == '') { 
-    $(searchField).animate({
-        width:'45%'
-        },400, function(){});
-    $(icon).animate({
-        right: '360px'
-        },400, function(){});
+    // Blur Event Handler
+    $(searchField).on('blur', function(){
+        if(searchField.val() == ''){ 
+            $(searchField).animate({
+                 width:'45%'
+            },400, function(){});
+            $(icon).animate({
+                right: '360px'
+            },400, function(){});
         }
     });
 
-// Prevents form from submitting  
-$('#search-form').submit(function(e){
-    e.preventDefault();
+    // Prevents automatic form submission 
+    $('#search-form').submit(function(e){
+      e.preventDefault();   
+        search();
     });
-
-});
-
+})
 
 function search() {
     // Clear Results
-    $('results').html('');
-    $('#buttons').html('');
+$('#results').html('');
+$('#buttons').html('');
 
-    // Get Form Input
-    q = $('#query').val();
+// Get Form Input
+q = $('#query').val();
 
-    //Run GET Request on API
-    $.get("https://www.googleapis.com/youtube/v3/activities", {
-        part: 'snippet, id',
-        q: q,
-        type: 'video',
-        key: 'AIzaSyCyLzWCMhLkN6g353fB6Vr6FE4TWhsgZpM'},
-        function (data){
-            var nextPageToken = data.nextPageToken;
-            var prevPageToken = data.prevPageToken;
-            console.log(data);
+// Run GET Request on API
 
-          
+$.get(
+  "https://www.googleapis.com/youtube/v3/search", {
+      part:'snippet, id',
+      q: q,
+      type: 'video',
+      key: 'AIzaSyCyLzWCMhLkN6g353fB6Vr6FE4TWhsgZpM'},
+      function(data){
+          var nextPageToken = data.nextPageToken;
+          var prevPageToken = data.prevPageToken;
 
-        }
-);
+          // Log Data
+          console.log(data);
+      }
+); 
 }
 
-    });
+
